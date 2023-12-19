9d7e8ef36e68cc6f984d65b1f0d56f555da65120
alejandrofernandezmarti <alefermar16@alu.edu.gva.es>
2023-12-19 18:41:01 +0100

    pilila

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
         <a href="/">Home</a>
 
         {{-- create todo --}}
-        <x-card title="To-Do Application">
+        <x-card title="Bebesitaaaaa">
             <form
                 action="{{ $model->exists ? route('todo.update', $model->id) : route('todo.store') }}"
                 method="POST"
