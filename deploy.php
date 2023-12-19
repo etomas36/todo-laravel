9e0081b2d7f816569891e7a1539e8240bdf4257a
Ethan Tomás <ethtomhor@alu.edu.gva.es>
2023-12-19 16:32:19 +0100

    [ADD] deploy works

---
+++ deploy.php
@@ -0,0 +1,38 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'https://github.com/etomas36/todo-laravel.git');
+
+add('shared_files', ['.env']);
+add('shared_dirs', ['storage', 'public/uploads','bootstrap/cache']);
+add('writable_dirs', ['bootstrap/cache', 'storage']);
+
+// Hosts
+
+host('172.16.221.102')
+    ->set('remote_user', 'ddaw-ud4-deployer')
+    ->set('ssh_multiplexing', false)
+    ->set('identityFile', 'C:\Users\sarza\.ssh\id_rsa')
+    ->set('deploy_path', '/var/www/ddaw-ud4-a4/html/todo-laravel');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
+
+//task('upload:env', function () {
+//    upload('.env.production', '{{deploy_path}}/shared/.env');
+//})->desc('Environment setup');
+   
+task('reload:php-fpm', function () {
+    run('sudo systemctl restart php8.1-fpm');
+});
+
+after('deploy', 'reload:php-fpm');
+
+task('composer:install', function () {
+    run('composer --working-dir=/var/www/prod-ud4-a4/html/todo-laravel/current install');
+});
